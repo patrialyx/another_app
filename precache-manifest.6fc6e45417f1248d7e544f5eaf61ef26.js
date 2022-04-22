30f18ebdd575d1b8891eb092d3cd84201bae6181
Patria Lim <patria.lyx@gmail.com>
2022-04-22 16:28:25 -0700

    Updates

---
+++ precache-manifest.6fc6e45417f1248d7e544f5eaf61ef26.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7233788d9e861da9228d",
+    "url": "/trazible-app/static/css/main.c6c284a1.chunk.css"
+  },
+  {
+    "revision": "7233788d9e861da9228d",
+    "url": "/trazible-app/static/js/main.7233788d.chunk.js"
+  },
+  {
+    "revision": "b33e30063011c2a213f4",
+    "url": "/trazible-app/static/css/1.cdf17d10.chunk.css"
+  },
+  {
+    "revision": "b33e30063011c2a213f4",
+    "url": "/trazible-app/static/js/1.b33e3006.chunk.js"
+  },
+  {
+    "revision": "7f26c8cb320c2c2cd840",
+    "url": "/trazible-app/static/js/runtime~main.7f26c8cb.js"
+  },
+  {
+    "revision": "530507c10c90da2399e285c052f34c00",
+    "url": "/trazible-app/static/media/besttree.530507c1.webp"
+  },
+  {
+    "revision": "ac3efff2a81a665dfda980b4ffb18483",
+    "url": "/trazible-app/static/media/live-oak.ac3efff2.jpg"
+  },
+  {
+    "revision": "3e714c1e03b9cc559825b7368a04cdeb",
+    "url": "/trazible-app/static/media/bald-cypress.3e714c1e.jpg"
+  },
+  {
+    "revision": "ecd466dad9cf19f6df682b2dd29471f3",
+    "url": "/trazible-app/index.html"
+  }
+];
